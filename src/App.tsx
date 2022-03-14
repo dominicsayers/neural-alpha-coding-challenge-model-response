23ec5205b32c2528e7602e29e39eb00c6693ab1e
Dominic Sayers <dominic@sayers.cc>
2022-03-14 16:21:32 +0000

    wip! add suggested Firebase config

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {initializeApp} from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyC3nWV-2y-UZOhFWHMq7fxKwF3LWZviizE',
+  authDomain: 'neural-alpha-coding-challenge.firebaseapp.com',
+  projectId: 'neural-alpha-coding-challenge',
+  storageBucket: 'neural-alpha-coding-challenge.appspot.com',
+  messagingSenderId: '909311194976',
+  appId: '1:909311194976:web:a0b0f19ac4dfd4705f5005',
+  measurementId: 'G-CEZPDYKPPS',
+};
+
+const app = initializeApp(firebaseConfig);
+
 function App() {
   return (
     <div className="App">
